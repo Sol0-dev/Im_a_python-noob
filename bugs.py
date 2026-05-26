e1dca8319cdfd97c167cb3bc7810a85bfc1dd4b3
Nico Lopez <lopeznico@scas.ph.education>
2026-05-26 17:12:21 +0800

    Create bugs.py

---
+++ bugs.py
@@ -0,0 +1,11 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+total = butterflies + beetles + ladybugs
+print('')
+print('I caught ', butterflies , ' 🦋 butterflies!')
+print('I caught ', beetles , ' 🪲 beetles!')
+print('I caught ', ladybugs, ' 🐞 ladybugs!')
+print('')
+print('I caught ' , int(total) , ' total bugs!')
